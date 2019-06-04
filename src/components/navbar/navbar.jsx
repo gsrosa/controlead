39eebcbdf064e3a9fff89ff8e5a3a79ab4fb57b1
gsrosa <gui.rosa.soares@gmail.com>
2019-06-04 10:34:07 -0300

    navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Container } from '../layout/container'
 
+const Nav = styled.nav`
+	width: 100%;
+	height: 60px;
+	background: #3d52bf;
+	display: flex;
+	line-height: 60px;
+	color: #fff;
+	justify-content: space-between;
+	padding: 0px 30px;
+	align-items: center;
+`
+
 export default ({ ...props }) => (
-	<Container>
-		<props.routes />
-	</Container>
+	<>
+		<Nav>
+			<h3>Logo</h3>
+			<h3>Logo</h3>
+		</Nav>
+		<Container>
+			<props.routes />
+		</Container>
+	</>
 )
